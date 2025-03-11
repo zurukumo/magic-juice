24ff9915366b2c685c1984f762457fa7a35a1e2b
zurukumo <zurukumo@gmail.com>
2025-03-11 16:44:17 +0900

    fix: bodyにも背景色を指定

--- app/root.tsx
+++ app/root.tsx
@@ -37,7 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-blue-500">
         {children}
         <ScrollRestoration />
         <Scripts />
