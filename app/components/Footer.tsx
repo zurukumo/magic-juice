01760105e2a7f0749d339742c8f25c0f19f6afbf
zurukumo <zurukumo@gmail.com>
2025-03-11 16:44:43 +0900

    fix: Footerのリンクを変更

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,8 @@ export const Footer = () => (
   <footer className="flex h-10 w-full items-center justify-center bg-blue-500">
     <div className="text-sm text-white lg:text-base">
       Powered by{' '}
-      <a href="https://x.com/zurukumo" target="_blank" className="underline hover:text-amber-50">
-        @zurukumo
+      <a href="https://zurukumo.dev" target="_blank" className="underline hover:text-amber-50">
+        zurukumo
       </a>
     </div>
   </footer>
